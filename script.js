0a19ebb959f24ec95f091719c362188636ec9f98
kazuha009 <168142302+kazuha009@users.noreply.github.com>
2024-05-24 19:47:51 -0300

    uploading frist version
    index.html
    script.js
    style.css

---
+++ script.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal ({
+    reset:true
+})
+
+sr.reveal('.area-1', {
+  duraction:2000,
+  rotate:{x:80, y:80, z:0}
+})
+
+window.sr = ScrollReveal ({
+    reset:true
+})
+
+sr.reveal('.area-2', {
+  duraction:2000,
+  rotate:{x:0, y:80, z:0}
+})
+
+window.sr = ScrollReveal ({
+    reset:true
+})
+
+sr.reveal('.area-3', {
+  duraction:2000,
+  rotate:{x:30, y:20, z:10000000000000000}
+})
+
